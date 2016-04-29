7f099b927920a0008340bd5e0b62010034f2107f
LiuBei <liubeitongxue@icloud.com>
2016-04-29 14:10:01 +0800

    提交了一个版本，但是执行时间超时了

--- LongestSubstringWithoutRepeatingCharacters/noReapt.py
+++ LongestSubstringWithoutRepeatingCharacters/noReapt.py
@@ -5,39 +5,75 @@ class Solution(object):
         :rtype: int
         """
         le = 0
-        d = dict()
-        lF = 0
-        flag = True
         count = 0
+        d = dict()
         if s == "":
             return 0
         else:
             l = list(s)
             le = 0
             for i in range(len(l)):
+                # 字母已经存在于dict中
                 if l[i] in d.keys():
                     count += 1
-                    leT = i - d[l[i]]
-                    if leT > le:
-                        le = leT
-                    d.pop(l[i])
-                    if lF >= le :
-                        le = lF
+                    indexLeft = d[l[i]]
+                    d[l[i]] = i
+                    indexRight = i
+                    l2 = list()
+                    l3 = list()
+                    leT = 0
+                    leT2 = 0
+                    for j in range(indexLeft,indexRight):
+                        l2.append(l[j])
+                    leT = len(l2)
+                    # print("i:{},indexLeft:{},dict:{}".format(i,indexLeft,d))
+                    # print(l2)
+                    a = indexLeft - 1
+                    while a >= 0:
+                        if l2.count(l[a]) == 0 :
+                            leT += 1
+                            a -= 1
+                        else:
+                            break
+                    # print(leT)
+                    #--
+                    if indexRight + 1 < len(l) and indexLeft + 1 < indexRight + 1:
+                        for k in range(indexLeft + 1,indexRight + 1):
+                            l3.append(l[k])
+                        leT2 = len(l3)
+                        # print(leT2)
+                        # print(indexRight)
+                        # print(len(l))
+                        if indexRight + 1 < len(l):
+                            for b in range(indexRight + 1,len(l)):
+                                # print("count{}".format(l3.count(l[b])))
+                                if l3.count(l[b]) == 0:
+                                    leT2 += 1
+                                else:
+                                    break
+                            # print(leT2)
+                    #--
+                    if leT >= leT2:
+                        if leT > le:
+                            le = leT
                     else:
-                        lF = 1
+                        if leT2 > le:
+                            le = leT2
+                # 字母还未存在于dict中
                 else:
                     d[l[i]] = i
-                    if flag:
-                        lF += 1
-                print('i:{},l[i]:{},d:{},len(l):{},le:{},lF:{}'.format(i,l[i],d,len(l),le,lF))
-        if lF > le and lF != 2:
-            le = lF
+
+                # print(i:{},l[i]:{},d:{},len(l):{},le:{}'.format(i,l[i],d,len(l),le))
+            if count == 0:
+                le = len(l)
         return le
 
 if __name__ == '__main__':
     s = Solution()
-    # s.lengthOfLongestSubstring("mxbeuyubuaupparvdhkeupipqkegovrdtuhnubrodlfslypp")
-    # s.lengthOfLongestSubstring("aab")
-    s.lengthOfLongestSubstring("abcabcbb")
-    # s.lengthOfLongestSubstring("eee")
-    # s.lengthOfLongestSubstring("cdd")
+    n = s.lengthOfLongestSubstring("mxbeuyubuaupparvdhkeupipqkegovrdtuhnubrodlfslypp")
+    # n = s.lengthOfLongestSubstring("aab")
+    # n = s.lengthOfLongestSubstring("abcabsbb")
+    # n = s.lengthOfLongestSubstring("eee")
+    # n = s.lengthOfLongestSubstring("cdd")
+    # n = s.lengthOfLongestSubstring("dvdf")
+    print(n)
